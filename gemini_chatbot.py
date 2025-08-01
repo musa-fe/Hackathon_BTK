5ed6df89ec6d88ad32b1534668a8a7c4e9f1b685
musa-fe <153774969+musa-fe@users.noreply.github.com>
2025-08-01 21:41:25 +0300

    step 3

--- gemini_chatbot.py
+++ gemini_chatbot.py
@@ -5,3 +5,22 @@ from dotenv import load_dotenv
 load_dotenv()
 
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+print("API KEY:", os.getenv("GOOGLE_API_KEY"))
+
+
+model = genai.GenerativeModel("models/gemini-2.5-pro")
+
+response = model.generate_content("Merhaba Gemini! Bana selam ver.")
+
+print("Gemini'den gelen cevap:")
+print(response.text)
+
+
+
+while True:
+    soru = input("Siz: ")
+    if soru.lower() in ["çik", "exit", "q"]:
+        break
+    yanit = model.generate_content(soru)
+    print("Gemini:", yanit.text)
