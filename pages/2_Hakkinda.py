7499dde73ed4fa280862c3888bdb83a5c9663215
musa-fe <153774969+musa-fe@users.noreply.github.com>
2025-08-04 14:36:20 +0300

    Update 2_Hakkinda.py

---
+++ pages/2_Hakkinda.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title("ℹ️ Hakkında")
+st.write("""
+Bu chatbot, dünyanın farklı ülkelerindeki fiyatları tahmin ederek  
+ucuz üretim - pahalı satış stratejinize yardımcı olur.  
+Hackathon ekibiniz tarafından geliştirilmiştir.
+""")
